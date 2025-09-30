e890df6489f3ad7f29d13b0634a3d830aa86116c
geovanaclira <geovana.liira@gmail.com>
2025-09-30 11:58:03 -0300

    feat: add modal in page listagem.html

--- js/listagem.js
+++ js/listagem.js
@@ -1,3 +1,5 @@
+let editIndex = null
+
 function atualizarLista() {
     const tabela = document.getElementById('tabela')
 
@@ -14,8 +16,12 @@ function atualizarLista() {
         <td>${item.origem}</td>
         <td>${item.validade}</td>
         <td>
-            <button onclick="editarItem(${index})">Editar</button>
-            <button onclick="removerItem(${index})">Excluir</button>
+           <button class="btn-editar" onclick="editarItem(${index})">
+                <i class="fa fa-edit"></i> <span>Editar</span
+            </button>
+            <button class="btn-excluir" onclick="removerItem(${index})">
+                <i class="fa fa-trash"></i> <span>Excluir</span>
+            </button>
         </td>
         `
         tabela.appendChild(novaLinha)
@@ -28,5 +34,43 @@ function removerItem(index) {
     localStorage.setItem("listaCompras", JSON.stringify(listaCompras))
     atualizarLista()
 }
+
+function editarItem(index) {
+    let listaCompras = JSON.parse(localStorage.getItem("listaCompras")) || []
+    const item = listaCompras[index]
+
+    editIndex = index
+
+    document.getElementById('edit-nome').value = item.nome
+    document.getElementById('edit-preco').value = item.preco
+    document.getElementById('edit-categoria').value = item.categoria
+    document.getElementById('edit-origem').value = item.origem
+    document.getElementById('edit-validade').value = item.validade
+
+    document.getElementById('modal').style.display = 'flex'
+}
+
+function fecharModal() {
+    document.getElementById('modal').style.display = 'none'
+}
+
+document.getElementById('form-editar').addEventListener('submit', function(e) {
+    e.preventDefault()
+
+    let listaCompras = JSON.parse(localStorage.getItem("listaCompras")) || []
+
+    listaCompras[editIndex] = {
+        nome: document.getElementById('edit-nome').value.trim(),
+        preco: document.getElementById('edit-preco').value.trim(),
+        categoria: document.getElementById('edit-categoria').value.trim(),
+        origem: document.getElementById('edit-origem').value.trim(),
+        validade: document.getElementById('edit-validade').value.trim()
+    }
+
+    localStorage.setItem("listaCompras", JSON.stringify(listaCompras))
+    fecharModal()
+    atualizarLista()
+})
+
 
 document.addEventListener("DOMContentLoaded", atualizarLista)
